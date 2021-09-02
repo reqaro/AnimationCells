c6fe9975b6ecce8cc9981530f9fbc080029a426f
Nikita Permogorcev <reqaro@gmail.com>
2021-09-03 00:46:47 +0300

    version 0.9.0

--- index.js
+++ index.js
@@ -19,34 +19,48 @@ const wrapItemsCreator = (items) => {
     func();
     return wrapItems.join('');
 };
-const wrapItems = wrapItemsCreator(16);
-$root.insertAdjacentHTML('beforeend', `<section class="wrapCell">${wrapItems}</section>`);
-$root.insertAdjacentHTML('beforeend', `<section id="console" class="console"></section>`);
-$root.insertAdjacentHTML('beforeend', `<div class="btn"><a id="start" class="start">Start</a></div>`);
-const $console = document.getElementById('console');
 const printConsole = (text) => {
     $console.insertAdjacentHTML('beforeend', `<p>${text} </p>`);
     $console.scrollTop = 9999;
 };
+const wrapItems = wrapItemsCreator(32);
+$root.insertAdjacentHTML('beforeend', `<section class="wrapCell">${wrapItems}</section>`);
+$root.insertAdjacentHTML('beforeend', `<section id="console" class="console"></section>`);
+$root.insertAdjacentHTML('beforeend', `<div class="btn"><a id="start" class="start">Start</a></div>`);
+const $console = document.getElementById('console');
 const $cells = document.getElementsByClassName('cell');
 const $startButton = document.getElementById('start');
+for (let elem of $cells) {
+    elem.addEventListener('animationend', () => {
+        printConsole(`${elem.classList[1]} animation <span class="animation_finish">finish</span>`);
+        if (elem === $cells[$cells.length - 1]) {
+            printConsole('---PROGRESS FINISH---');
+            $startButton.innerText = 'Start';
+            for (let elem of $cells) {
+                elem.classList.remove('cell__anim');
+            }
+        }
+    });
+}
+let animationTime = 100;
+$root.insertAdjacentHTML('beforeend', `<button type='button' id="changeAnimationTimeBtn">100 ms</button>`);
+const $changeAnimationTimeBtn = document.getElementById('changeAnimationTimeBtn');
+$changeAnimationTimeBtn.addEventListener('click', () => {
+    animationTime === 100
+        ? animationTime = 1000
+        : animationTime = 100;
+    $changeAnimationTimeBtn.innerText = `${animationTime} ms`;
+});
 $startButton.addEventListener('click', () => __awaiter(void 0, void 0, void 0, function* () {
     printConsole('---PROGRESS START---');
-    $startButton.innerText = 'in progress';
+    $startButton.innerText = 'In progress';
     for (let elem of $cells) {
         yield new Promise((resolve) => {
             setTimeout(() => {
                 elem.classList.add('cell__anim');
                 printConsole(`${elem.classList[1]} animation <span class="animation_start">start</span>`);
                 resolve();
-            }, 100);
-        }).then(() => setTimeout(() => printConsole(`${elem.classList[1]} animation <span class="animation_finish">finish</span>`), 400));
+            }, animationTime);
+        });
     }
-    setTimeout(() => {
-        $startButton.innerText = 'Start';
-        printConsole('---PROGRESS FINISH---');
-        for (let elem of $cells) {
-            elem.classList.remove('cell__anim');
-        }
-    }, 1000);
 }));
--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 const $root = <HTMLDivElement>document.getElementById('root')
-const wrapItemsCreator = (items: number) => {  //функция принимающая кол-во(number) элементов `<div class="cell cell__${i + 1}">${i + 1}</div>` которые будут помещены в массив `wrapItems`
+
+const wrapItemsCreator = (items: number) => {  //функция принимающая кол-во(number) элементов `<div class="cell cell__${i + 1}">${i + 1}</div>` которые будут помещены в массив wrapItems
     const wrapItems: Array<string> = []        //массив для хранения элементов
     let func = () => {
         for (let i = 0; i < items; i++) {
@@ -9,38 +10,59 @@ const wrapItemsCreator = (items: number) => {  //функция принимаю
     func()
     return wrapItems.join('')
 }
-const wrapItems = wrapItemsCreator(16) // 16 32 64 ну и т.д
-$root.insertAdjacentHTML('beforeend', `<section class="wrapCell">${wrapItems}</section>`) // вставляем все элементы из `wrapItems`
+const printConsole = (text: string) => {  // функция принимающая сообщение(string) которое будет отображено в конце блока console и помещено в <p></p>
+    $console.insertAdjacentHTML('beforeend', `<p>${text} </p>`)
+    $console.scrollTop = 9999 // автоскролл вниз при вызове функции `printConsole` //scrollBy(0,20)
+}
+
+const wrapItems = wrapItemsCreator(32) // 16 32 64
+$root.insertAdjacentHTML('beforeend', `<section class="wrapCell">${wrapItems}</section>`) // вставляем все элементы из wrapItems
 $root.insertAdjacentHTML('beforeend', `<section id="console" class="console"></section>`) // создаем блок с консолью
 $root.insertAdjacentHTML('beforeend', `<div class="btn"><a id="start" class="start">Start</a></div>`) // создаем кнопку Start
 const $console = <HTMLElement>document.getElementById('console')
-const printConsole = (text: string) => {  // функция принимающая сообщение(string) которое будет отображено в конце блока console и помещено в `<p></p>`
-    $console.insertAdjacentHTML('beforeend', `<p>${text} </p>`)
-    $console.scrollTop = 9999 // автоскролл вниз при вызове функции `printConsole`
-}
+
 const $cells = <HTMLCollection>document.getElementsByClassName('cell')
 const $startButton = <HTMLLinkElement>document.getElementById('start')
+
+
+for (let elem of $cells) {           // каждому элементу из $cells добавляем слушатель animationend
+    elem.addEventListener('animationend', () => {
+        printConsole(`${elem.classList[1]} animation <span class="animation_finish">finish</span>`) // сигнал в консоль, что анимация элемента elem завершена
+        if (elem === $cells[$cells.length - 1]) {
+            printConsole('---PROGRESS FINISH---')  // сигнализируем в консоль что анимация всех элементов завершена
+            $startButton.innerText = 'Start' // меняем кнопку на Start
+            for (let elem of $cells) {
+                elem.classList.remove('cell__anim') // удаляем класс с анимацией у всех элементов $cells
+            }
+        }
+    })
+}
+
+let animationTime = 100
+$root.insertAdjacentHTML('beforeend', `<button type='button' id="changeAnimationTimeBtn">100 ms</button>`)
+const $changeAnimationTimeBtn = <HTMLLinkElement>document.getElementById('changeAnimationTimeBtn')
+
+$changeAnimationTimeBtn.addEventListener('click', () => {
+    animationTime === 100
+        ? animationTime = 1000
+        : animationTime = 100
+    $changeAnimationTimeBtn.innerText = `${animationTime} ms`
+})
+
+
 $startButton.addEventListener('click', async () => { /// async
     printConsole('---PROGRESS START---')  // сигнал о начале выполнения функции
-    $startButton.innerText = 'in progress'   // кнопка Start -> in progress
+    $startButton.innerText = 'In progress'   // кнопка Start -> In progress
     for (let elem of $cells) {
-        await new Promise((resolve: any) => { /// ждём исполнения функции resolve
+        await new Promise((resolve: any) => {
             setTimeout(() => {
                 elem.classList.add('cell__anim') // добовление класса с анимацией
                 printConsole(`${elem.classList[1]} animation <span class="animation_start">start</span>`) // сигнал в консоль о начале анимации у блока elem
-                resolve()
-            }, 100)
-        }).then(() => setTimeout(() => printConsole(`${elem.classList[1]} animation <span class="animation_finish">finish</span>`), 400)) // сигнал об окончании анимации. По таймеру
+                resolve()  // ждём исполнения функции resolve
+            }, animationTime)
+        })
     }
-    setTimeout(() => { // костыль который обязательно должет имень задержку >800,  иначе выполнение будет быстрее чем закончится .then выше. Не знаю как сделать это нрмально
-        $startButton.innerText = 'Start'   // кнопка in progress -> Start
-        printConsole('---PROGRESS FINISH---') // сигнал об окончании работы программы
-        for (let elem of $cells) {
-            elem.classList.remove('cell__anim') // удаление класса с анимацией у всех элементов коллекции $cells
-        }
-    }, 1000)
 })
-
 
 
 
